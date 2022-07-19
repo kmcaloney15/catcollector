cceae40db4cec2111663bdd5d046d5b8d4e3c146
kmcaloney15 <katymcaloney@gmail.com>
2022-07-19 10:48:37 -0400

    next day steps

--- main_app/views.py
+++ main_app/views.py
@@ -2,25 +2,7 @@ from django.shortcuts import render
 
 # Add the following import
 from django.http import HttpResponse
-
-# -------- CLASS -----------------
-
-# Add the Cat class & list and view function below the imports
-class Cat:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-cats = [
-  Cat('Lolo', 'tabby', 'foul little demon', 3),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
-
-
-
+from models import Cat
 
 #  -------- DEFINE VIEWS -----------------
 
@@ -33,4 +15,5 @@ def about(request):
 
 # Add new view
 def cats_index(request):
-    return render(request, 'cats/index.html', {'cats': cats}) #creating dictionary / object - cats are equal to cats
+  cats = Cat.objects.all()
+  return render(request, 'cats/index.html', { 'cats': cats }) #creating dictionary / object - cats are equal to cats
