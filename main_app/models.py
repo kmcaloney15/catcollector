2861229817c2f6f759bd271817ca46d4c5f4271a
kmcaloney15 <katymcaloney@gmail.com>
2022-07-20 11:13:33 -0400

    almost lunch

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # A tuple of 2-tuples
 MEALS = (
@@ -21,10 +22,15 @@ class Cat(models.Model):
     def __str__(self):
         return self.name
 
+    # Add this method
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'cat_id': self.id})
+
+
 
 # Add new Feeding model below Cat model
 class Feeding(models.Model):   
-    date = models.DateField()
+    date = models.DateField('feeding date')
     meal = models.CharField(
         max_length=1,
         # add the 'choices' field option
@@ -32,4 +38,21 @@ class Feeding(models.Model):
         # set the default value for meal to be 'B'
         default=MEALS[0][0]
     )
+
+    # Create a cat_id FK
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    # As you can see, the ForeignKey field-type is used to create a one-to-many relationship. The first argument provides the parent Model.
+
+    # In a one-to-many relationship, the on_delete=models.CASCADE is required. It ensures that if a Cat record is deleted, all of the child Feedings will be deleted automatically as well - thus avoiding orphan records (seriously, that's what they're called).
+
+
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        # so we know what meal we are having on what date
+        return f"{self.get_meal_display()} on {self.date}"
+        # Check out the convenient get_meal_display() method Django automagically creates to access the human-friendly value of a Field.choice like we have on meal.
+
+
+
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,31 +1,53 @@
 from django.shortcuts import render
-
 # Add the following import
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView
 from .models import Cat
+from django.http import HttpResponse
+# Add UdpateView & DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
+
+
 
 #  -------- DEFINE VIEWS -----------------
 
 # Define the home view
-def home(request): #request is like .req in express 
+
+
+def home(request):  # request is like .req in express
     # return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>') # this is like res.send in express
     return render(request, 'home.html')
 
+
 def about(request):
-    return render(request, 'about.html') # render looks for templates
+    return render(request, 'about.html')  # render looks for templates
+
 
 # Add new view
 def cats_index(request):
   cats = Cat.objects.all()
-  return render(request, 'cats/index.html', { 'cats': cats }) #creating dictionary / object - cats are equal to cats
+  # creating dictionary / object - cats are equal to cats
+  return render(request, 'cats/index.html', {'cats': cats})
 
 
 def cats_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
-  return render(request, 'cats/detail.html', { 'cat': cat })
+  feeding_form = FeedingForm()
+  return render(request, 'cats/detail.html', {'cat': cat, 'feeding_form': feeding_form})
 
 
-# def CatCreate(request, cat_id):
-#     cat = Cat.objects.get(id=cat_id)
-#     return HttpResponse(f"Filler cat create page")
-    # return render(request, 'cats/create.html', { 'cat': cat })
+class CatCreate(CreateView):
+  model = Cat
+  fields = '__all__'
+  success_url = '/cats/'
+
+
+class CatUpdate(UpdateView):
+  model = Cat
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['breed', 'description', 'age']
+
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats/'
